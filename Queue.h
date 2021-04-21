991a67453617623a8c14a67db17e1f92ea8cf136
Bhargavi <82858718+Bhargavi-hash@users.noreply.github.com>
2021-04-21 19:48:13 +0530

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -11,15 +11,15 @@ typedef struct que* PtrToQue;
 //queue with doubly linked list implementation.
 struct que
 {
-    elem_type X1, Y1; // Initial position of the coin
-    elem_type X2, Y2; // Final position of the coin
+    elem_type X1; // Initial position of the coin
+    elem_type X2; // Final position of the coin
     PtrToQue Link_To_Next;
     PtrToQue Link_To_Previous;
 };
 
-PtrToQue MakeNode(elem_type X1, elem_type X2, elem_type Y1, elem_type Y2);
+PtrToQue MakeNode(elem_type X1, elem_type X2);
 
-void Push(elem_type X1, elem_type X2, elem_type Y1, elem_type Y2);
+void Push(elem_type X1, elem_type X2);
 void Pop();
 
 #endif

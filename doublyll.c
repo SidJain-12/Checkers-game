48aee42f26d963c2b886dfcf80deb3500af4d740
YUG-DEDHIA <81165790+YUG-DEDHIA@users.noreply.github.com>
2021-04-27 16:15:45 +0530

    Add files via upload

---
+++ doublyll.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "doublyll.h"
+
+void push(struct Node **head_ref, int new_data)
+{
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    new_node->prev = NULL;
+    if ((*head_ref) != NULL)
+        (*head_ref)->prev = new_node;
+    (*head_ref) = new_node;
+}
+void append(struct Node **head_ref, int new_data)
+{
+
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+
+    struct Node *last = *head_ref;
+
+    new_node->data = new_data;
+
+    new_node->next = NULL;
+
+    if (*head_ref == NULL)
+    {
+        new_node->prev = NULL;
+        *head_ref = new_node;
+        return;
+    }
+
+    while (last->next != NULL)
+        last = last->next;
+
+    last->next = new_node;
+
+    new_node->prev = last;
+
+    return;
+}

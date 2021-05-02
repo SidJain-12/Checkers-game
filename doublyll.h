4856be7289718857576d1623b1ca38a353459a2f
SiddharthJain-12 <71603742+SiddharthJain-12@users.noreply.github.com>
2021-05-02 16:00:26 +0530

    Update doublyll.h

--- doublyll.h
+++ doublyll.h
@@ -1,11 +1,15 @@
 #ifndef __DOUBLYLL_H
 #define __DOUBLYLL_H
-typedef struct Node node;
-struct Node {
-    int data;
-    struct Node* next; // Pointer to next node in DLL
-    struct Node* prev; // Pointer to previous node in DLL
+#include "game.h"
+
+struct Node
+{
+    GameState G;
+    struct Node *next; // Pointer to next node in DLL
+    struct Node *prev; // Pointer to previous node in DLL
 };
-void push(struct Node** head_ref, int new_data);
-void append(struct Node** head_ref, int new_data);
+
+struct Node* GetNewNode(GameState G);
+void push(GameState G);
+
 #endif

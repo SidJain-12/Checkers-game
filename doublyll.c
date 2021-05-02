4b07c233b96972bc9586c78c131164fe34873e9e
SiddharthJain-12 <71603742+SiddharthJain-12@users.noreply.github.com>
2021-05-02 15:59:57 +0530

    Update doublyll.c

--- doublyll.c
+++ doublyll.c
@@ -1,41 +1,24 @@
 #include <stdio.h>
 #include "doublyll.h"
 
-void push(struct Node **head_ref, int new_data)
+struct Node* GetNewNode(GameState G) 
 {
-    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
-
-    new_node->data = new_data;
-    new_node->next = (*head_ref);
-    new_node->prev = NULL;
-    if ((*head_ref) != NULL)
-        (*head_ref)->prev = new_node;
-    (*head_ref) = new_node;
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	newNode->GameState = G;
+	newNode->prev = NULL;
+	newNode->next = NULL;
+	return newNode;
 }
-void append(struct Node **head_ref, int new_data)
+
+void push(GameState G)
 {
-
-    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
-
-    struct Node *last = *head_ref;
-
-    new_node->data = new_data;
-
-    new_node->next = NULL;
-
-    if (*head_ref == NULL)
-    {
-        new_node->prev = NULL;
-        *head_ref = new_node;
-        return;
-    }
-
-    while (last->next != NULL)
-        last = last->next;
-
-    last->next = new_node;
-
-    new_node->prev = last;
-
-    return;
+	struct Node* temp = head;
+	struct Node* newNode = GetNewNode(G);
+	if(head == NULL) {
+		head = newNode;
+		return;
+	}
+	while(temp->next != NULL) temp = temp->next; // Go To last Node
+	temp->next = newNode;
+	newNode->prev = temp;
 }

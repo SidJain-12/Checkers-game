bef9def72ac79745accc4c8d6f69594c3305ea9b
Siddharth jain <71603742+SiddharthJain-12@users.noreply.github.com>
2021-05-04 23:16:53 +0530

    Update doublyll.c

--- doublyll.c
+++ doublyll.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "doublyll.h"
+#include "game.h"
 
-struct Node* GetNewNode(GameState G) 
+// struct Node *head;
+
+struct Node *GetNewNode(GameState G)
 {
-	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-	newNode->G = G;
-	newNode->prev = NULL;
-	newNode->next = NULL;
-	return newNode;
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->G = G;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
 }
 
-void push(GameState G)
+void insert(GameState G, struct Node *head)
 {
-	struct Node* temp = head;
-	struct Node* newNode = GetNewNode(G);
-	if(head == NULL) {
-		head = newNode;
-		return;
-	}
-	while(temp->next != NULL) temp = temp->next; // Go To last Node
-	temp->next = newNode;
-	newNode->prev = temp;
+    struct Node *temp = head;
+    struct Node *newNode = GetNewNode(G);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    while (temp->next != NULL)
+        temp = temp->next; // Go To last Node
+    temp->next = newNode;
+    newNode->prev = temp;
 }

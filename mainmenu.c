4e626ec7784bfc350d1fcc657c54a680459901be
Siddharth jain <71603742+SiddharthJain-12@users.noreply.github.com>
2021-05-05 11:45:41 +0530

    Update mainmenu.c
    Updated main menu for Undo, Review and Show all moves.

--- mainmenu.c
+++ mainmenu.c
@@ -1,70 +1,86 @@
-#include "game.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-
-
-#include <stdio.h>
 #include "game.h"
+#include "doublyll.h"
 
-int main(void){
-
+int main(void)
+{
     int status = 0;
     GameState G = initEmptyBoard();
     setBoard(G);
     displayBoardClear(G);
-    char operation[100]
-    scanf("%s",operation);
-    if(strcmp(operation,"movepiece")==0)
-    {
-    printf("Enter x and y coordinates of the piece you want to move and the target square\n");
+    struct Node *head = GetNewNode(G);
 
-    int x=0,y=0,tX=0,tY=0;
-
+    int x = 0, y = 0, tX = 0, tY = 0;
 
     //need to include a provision to restrict moves if a capture is available
-    while(1){
-        if(!scanf(" %d %d %d %d",&x,&y,&tX,&tY))
-            break;
-        x--; y--; tX--; tY--;
+    while (1)
+    {
+        printf("What do you wanna do: Undo,Review,Move or Exit?");
+        char str[100];
+        scanf("%s", str);
+        if (strcmp(str, "UNDO") == 0)
+        {
+            G = Undo(head);
+            displayBoardClear(G);
+            continue;
+        }
+        if (strcmp(str, "REVIEW") == 0)
+        {
+            Review(head);
+            continue;
+        }
+        if (strcmp(str, "MOVE") == 0)
+        {
+            printf("Enter x and y coordinates of the piece you want to move and the target square\n");
+        }
+        if (strcmp(str, "Show_Possible_moves") == 0)
+        {
+            //func
+            continue;
+        }
 
-        if(x==-1 && y==-1 && tX==-1 && tY==-1)
-            break;
-        
+        if (strcmp(str, "EXIT") == 0)
+        {
+            return 0;
+        }
 
-        status = movePiece(G,x,y,tX,tY);
+        if (!scanf(" %d %d %d %d", &x, &y, &tX, &tY))
+            break;
+
+        x--;
+        y--;
+        tX--;
+        tY--;
+
+        if (x == -1 && y == -1 && tX == -1 && tY == -1)
+        {
+            free(head);
+            break;
+        }
+
+        status = movePiece(G, x, y, tX, tY);
+        insert(G, head);
         displayBoardClear(G);
-        switch(status){
-            default:
-            case 0:
-                printf("Invalid move\n");
-                break;
-            case -1:
-                printf("You must make a capture\n");
-                break;
-            case 1:
-                printf("You made a regular move\n");
-                break;
-            case 2:
-                printf("You captured a piece, you must make another capture if possible\n");
-                break;
-            case 3:
-                printf("You promoted a pawn\n");
-                break;
-
+        switch (status)
+        {
+        default:
+        case 0:
+            printf("Invalid move\n");
+            break;
+        case -1:
+            printf("You must make a capture\n");
+            break;
+        case 1:
+            printf("You made a regular move\n");
+            break;
+        case 2:
+            printf("You captured a piece, you must make another capture if possible\n");
+            break;
+        case 3:
+            printf("You promoted a pawn\n");
+            break;
         }
     }
-    }
-    else if(strcmp(operation,"review")==0)
-    {
-    //review func//
-    }
-    else if(strcmp(operation,"undo")==0)
-    {
-    //undo func
-    }
-    else if(strcmp(operation,"Show_Possible_moves")==0)
-    {
-    //func
-    }
-    
 }
